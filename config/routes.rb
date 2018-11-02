5ad9fcce2a30cd1120d5b1b3389acfb11dbd51e5
ivan.arias <ivanarias13@gmail.com>
2018-11-02 12:03:59 -0600

    implemeted validatios

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,6 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
 
 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
