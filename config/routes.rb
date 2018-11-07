23de77f6d22823c1ccef6244ffd1ba16b1b82680
ivan.arias <ivanarias13@gmail.com>
2018-11-07 13:40:40 -0600

    poniendo al dia algo se perdio, rutas malas

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show]
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-  #this comes by default
-  #get 'pages/home'
-  #get 'pages/about'
-  #get 'pages/contact'
-  #################
+
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
   resources :blogs do
     member do
       get :toggle_status
-     end
+    end
   end
-  #adding this on class 64 custon routes
 
   root to: 'pages#home'
-  get 'about', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
-
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
